0272fe45ed3f66ca209901f1324e590e06dc0e90
Tejas Shetty <tejasshetty.1808@gmail.com>
2020-02-25 19:47:11 +0530

    Blacken mu_diff.py

--- src/mu_diff/mu_diff.py
+++ src/mu_diff/mu_diff.py
@@ -4,5 +4,5 @@ from numpy import absolute
 def mu_diff_maker(mu_end_targ, mu_end_cal):
     delta_mu_end = mu_end_targ - mu_end_cal
     # mu_difference = sum(abs(dme)**2 for dme in delta_mu_end)
-    mu_difference = sum(absolute(delta_mu_end*delta_mu_end))
+    mu_difference = sum(absolute(delta_mu_end * delta_mu_end))
     return mu_difference
