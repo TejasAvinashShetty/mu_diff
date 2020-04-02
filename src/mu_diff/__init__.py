d6f6b4e37f63836ee598c27f3fc769bd95863eec
Tejas Shetty <tejasshetty.1808@gmail.com>
2020-04-02 20:44:30 +0530

    Expose mu_diff_maker
    This is to enable pytes to discover tests.

--- src/mu_diff/__init__.py
+++ src/mu_diff/__init__.py
@@ -1,8 +1,13 @@
 """Top-level package for mu_diff."""
 
-__version__ = '0.1.0-dev'
+__version__ = "0.1.0-dev"
 
 # All members whose name does not start with an underscore must be listed
 # either in __all__ or in __private__
-__all__ = []
+from . import mu_diff
+
+__all__ = [
+    "mu_diff",
+]
+
 __private__ = []
