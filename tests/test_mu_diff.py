af1d434114fe0ab7f914b888527c80d678c72b08
Tejas Shetty <tejasshetty.1808@gmail.com>
2020-04-02 20:50:01 +0530

    Add first non trivial test.
    Add test that mu_diff_maker should return zero
    if given two similar 1d arrays.

--- tests/test_mu_diff.py
+++ tests/test_mu_diff.py
@@ -2,7 +2,10 @@
 
 import pytest
 from pkg_resources import parse_version
+from numpy.random import random_sample
+from numpy.testing import assert_allclose
 
+# from mu_diff import mu_diff_maker
 import mu_diff
 
 
@@ -11,3 +14,8 @@ def test_valid_version():
     v_curr = parse_version(mu_diff.__version__)
     v_orig = parse_version("0.1.0-dev")
     assert v_curr >= v_orig
+
+
+def test_mu_diff():
+    a = random_sample((10,))
+    assert mu_diff.mu_diff.mu_diff_maker(a, a) == 0
